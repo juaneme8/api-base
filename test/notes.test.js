8a18845ed6ccdebbbbe97ba4df91374603e7ec43
Juan Ocho <juaneme8@gmail.com>
2021-10-05 18:24:48 -0300

    GET and POST requests test

--- test/notes.test.js
+++ test/notes.test.js
@@ -6,51 +6,88 @@ const Note = require('../models/Note');
 const api = supertest(app);
 
 const initialNotes = [
-    {
-        content: 'Nota 1',
-        important: true,
-        date: new Date()
-    },
-    {
-        content: 'Nota 2',
-        important: true,
-        date: new Date()
-    },
-    {
-        content: 'Nota 3',
-        important: true,
-        date: new Date()
-    }
-]
+	{
+		content: 'Notas de Juan',
+		important: true,
+		date: new Date(),
+	},
+	{
+		content: 'Notas de Paco',
+		important: true,
+		date: new Date(),
+	},
+	{
+		content: 'Notas de Pedro',
+		important: true,
+		date: new Date(),
+	},
+];
 
 beforeEach(async () => {
-    await Note.deleteMany({});
+	await Note.deleteMany({});
 
-    const note1 = new Note(initialNotes[0])
-    await note1.save()
+	const note1 = new Note(initialNotes[0]);
+	await note1.save();
 
-    const note2 = new Note(initialNotes[1])
-    await note2.save()
+	const note2 = new Note(initialNotes[1]);
+	await note2.save();
 
-    const note3 = new Note(initialNotes[2])
-    await note3.save()
-})
+	const note3 = new Note(initialNotes[2]);
+	await note3.save();
+});
 
 describe('GET /api/notes', () => {
-    test('notes are returned in json', async () => {
-        await api
-            .get('/api/notes')
-            .expect(200)
-            .expect('Content-Type', /application\/json/)
-    });
-    test('there are two notes', async () => {
-        const res = await api.get('/api/notes')
-        expect(res.body).toHaveLength(initialNotes.length);
-    });
-})
+	test('the result should be a json', async () => {
+		await api
+			.get('/api/notes')
+			.expect(200)
+			.expect('Content-Type', /application\/json/);
+	});
+	test('there should be two notes', async () => {
+		const res = await api.get('/api/notes');
+		expect(res.body).toHaveLength(initialNotes.length);
+	});
 
+	test('the first note should be about Juan', async () => {
+		const res = await api.get('/api/notes');
+
+		expect(res.body[0].content).toBe('Notas de Juan');
+	});
+	test('there should be a note about Paco', async () => {
+		const res = await api.get('/api/notes');
+
+		const contents = res.body.map(note => note.content);
+
+		expect(contents).toContain('Notas de Paco');
+	});
+	test('a new note should be added', async () => {
+		const newNote = {
+			content: 'Notas de De La Mar',
+			important: false,
+		};
+
+		await api
+			.post('/api/notes')
+			.send(newNote)
+			.expect(200)
+			.expect('Content-Type', /application\/json/);
+
+		const res = await api.get('/api/notes');
+
+		const contents = res.body.map(note => note.content);
+
+		expect(contents).toContain('Notas de De La Mar');
+		expect(res.body).toHaveLength(initialNotes.length + 1);
+	});
+	test('an empty note should not be added', async () => {
+		const newNote = {
+			important: false,
+		};
+		await api.post('/api/notes').send(newNote).expect(200);
+	});
+});
 
 afterAll(() => {
-    server.close()
-    mongoose.connection.close();
-})
+	server.close();
+	mongoose.connection.close();
+});
