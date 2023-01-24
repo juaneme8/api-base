2a908e22ed640b94e9b40d31e941ad1b828152cd
Juan Ocho <juaneme8@gmail.com>
2023-01-24 07:40:58 -0300

    solve strictQuery warning

--- mongo.js
+++ mongo.js
@@ -1,22 +1,19 @@
 const mongoose = require('mongoose')
 
-const { MONGO_DB_URI, MONGO_DB_URI_TEST, NODE_ENV } = process.env
+const {MONGO_DB_URI, MONGO_DB_URI_TEST, NODE_ENV} = process.env
 
-const connectionString = NODE_ENV === 'test'
-    ? MONGO_DB_URI_TEST
-    : MONGO_DB_URI
+const connectionString = NODE_ENV === 'test' ? MONGO_DB_URI_TEST : MONGO_DB_URI
 
-mongoose.connect(connectionString, {
+mongoose.set('strictQuery', false)
+mongoose
+  .connect(connectionString, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-})
-    .then(() =>
-        console.log('Database Connected')
-    ).catch(err =>
-        console.error(err)
-    )
+  })
+  .then(() => console.log('Database Connected'))
+  .catch(err => console.error(err))
 
 process.on('uncaughtException', error => {
-    console.error(error)
-    mongoose.disconnect()
+  console.error(error)
+  mongoose.disconnect()
 })
